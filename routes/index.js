31ab9fb89b8777698e1a87adbb449a714d4502cd
tangxiao <tangxiao@youzan.com>
2016-10-20 18:13:13 +0800

    添加注册页面

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,9 @@ module.exports = function (app) {
   app.get('/login', function (req, res, next) {
     res.render('login', { title: 'Express' });
   })
+  app.get('/register', function (req, res, next) {
+    res.render('register', { title: 'Express' });
+  })
   app.get('/users', function (req, res, next) {
     res.send('respond with a resource');
   })
