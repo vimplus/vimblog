34a3b061a8c1968c44dc5ee3550dc4ec6e5f2426
tangxiao <tangxiao@youzan.com>
2016-10-27 21:01:35 +0800

    增加404页

--- app.js
+++ app.js
@@ -43,11 +43,17 @@ app.use(session({
 routes(app);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+/*app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
-});
+});*/
+app.use(function (req, res) {
+  res.render('404', {
+    title: '404',
+    user: req.session.user
+  })
+})
 
 // error handlers
 
