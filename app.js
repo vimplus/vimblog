4927ea1998ebf89ba1d535838ec117a883148287
tangxiao <tangxiao@youzan.com>
2016-10-28 14:55:14 +0800

    z增加日志功能

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ var config = require('./config/config');
 var mongodb = require('./config/mongoose');
 var flash = require('connect-flash');
 
+var fs = require('fs');
+var accessLog = fs.createWriteStream('access.log', {flags: 'a'});
+var errorLog = fs.createWriteStream('error.log', {flags: 'a'});
+
 var db = mongodb();
 var app = express();
 
@@ -23,10 +27,17 @@ app.set('view engine', 'ejs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(flash());
 app.use(logger('dev'));
+app.use(logger({stream: accessLog}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function (err, req, res, next) {
+  var meta = '[' + new Date() + '] ' + req.url + '\n';
+  errorLog.write(meta + err.stack + '\n');
+  next();
+});
 
 app.use(session({
   secret: config.cookieSecret,
